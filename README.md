7844b4e8c4772310df92ded375a278d472d18fe9
PurpleLiger <smmz92@gmail.com>
2024-02-27 21:56:00 -0500

    plan functions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # C3_network_scanner
+
+This project aims to create a network scanning utility that has functions enabling the following functionality:
+- Scanning of a host (IP address) and a port to scan if it is open.
+- Get information about the host such as any aliases and their IPs.
+- Conduct a ping sweep to find live hosts in an IP network range.
--- network_scanner.py
+++ network_scanner.py
@@ -7,4 +7,18 @@ def scan_host(host, port):
     """
     Scan the host and port provided to check if it is open.
     """
+
+
+def get_host_info(host):
+    """
+    Gather host IP and if aliases are present, retrieve their IP as well.
+    """
+
+
+def scan_network(network):
+    """
+    Scan the network to detect any hosts in an IP range.
+    """
     
+if __name__ == "__main__":
+    main()
